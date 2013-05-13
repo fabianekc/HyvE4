ca197feeb5dba9e2905351ec521d210d3d07b378
root <root@v22012114971199.netcup.net>
2013-05-13 22:18:14 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hyve4::Application.config.secret_token = 'b2ccc37faab49e0664383efd302ce36de6452f91b289889d1fefa5056dfe9a14fff4f29b156bbac87f0d05d35838538c683f4d9e031ecaf89567ac6d1c45f649'
