ecb3727889dd93106006f2dde647ad179d0d1348
root <root@v22012114971199.netcup.net>
2013-05-13 22:35:05 +0200

    Add a StaticPages controller

---
+++ README.md
@@ -0,0 +1,4 @@
+# Ruby on Rails: HyvE4
+
+This is the first implementation of Hyve in Ruby on Rails
+by Christoph Fabianek, Founder [Hyve.me](http://hyve.me/)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Hyve4::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/imprint"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
