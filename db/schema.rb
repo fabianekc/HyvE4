cb507985c25544d71d4b27ba6c7e9bf94553bc09
Christoph Fabianek <christoph.fabianek@gmail.com>
2013-09-12 12:45:04 +0200

    new project field nextmail

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130905213500) do
+ActiveRecord::Schema.define(:version => 20130912104227) do
 
   create_table "datavals", :force => true do |t|
     t.string   "value"
@@ -85,6 +85,7 @@ ActiveRecord::Schema.define(:version => 20130905213500) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.boolean  "emaildata"
+    t.datetime "nextmail"
   end
 
   add_index "projects", ["user_id"], :name => "index_projects_on_user_id"
